cc1cb4394e14c77694c1e39efc5714be1a5bd1b2
PiyuMore98 <poojamore25041998@gmail.com>
2022-08-15 11:38:26 +0530

    Project added to central repository

---
+++ src/test/java/com/softTech/GitBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
